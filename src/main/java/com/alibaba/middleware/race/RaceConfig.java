26605b54e6bc7d04df2d87b116ddcee3fe307751
wxyBUPT <746451950@qq.com>
2016-06-30 15:00:59 +0800

    更改配置项，打算在本地的拓扑中执行

--- src/main/java/com/alibaba/middleware/race/RaceConfig.java
+++ src/main/java/com/alibaba/middleware/race/RaceConfig.java
@@ -20,15 +20,15 @@ public class RaceConfig implements Serializable{
     public static String MqTmallTradeTopic = "MiddlewareRaceTestData_TMOrder";
     public static String MqTaoboaTradeTopic = "MiddlewareRaceTestData_TBOrder";
 
-    public static String TairConfigServer = "10.101.72.127:5198";
-    public static String TairSalveConfigServer = "10.101.72.128:5198";
-    public static String TairGroup = "group_tianchi";
-    public static Integer TairNamespace = 25395;
+    //public static String TairConfigServer = "10.101.72.127:5198";
+    //public static String TairSalveConfigServer = "10.101.72.128:5198";
+    //public static String TairGroup = "group_tianchi";
+    //public static Integer TairNamespace = 25395;
 
-    //public static String TairConfigServer = "115.28.93.106:5198";
-    //public static String TairSalveConfigServer =null;
-    //public static String TairGroup = "group_1";
-    //public static Integer TairNamespace = 0;
+    public static String TairConfigServer = "115.28.93.106:5198";
+    public static String TairSalveConfigServer =null;
+    public static String TairGroup = "group_1";
+    public static Integer TairNamespace = 0;
 
     //测试环境使用配置
     public static boolean isConsumerFromFirstOffset = true;
@@ -41,9 +41,9 @@ public class RaceConfig implements Serializable{
 
     //下面的参数是用于本地测试的
     //redis 中保存pc mb 每分钟交易额 的所有key 值的key
-    public static String KeySetForTmTb= "newRandsssdddddodddiiiiiKeyNewwwwww";
-    public static String KeySetForRatio = "newdddRssssandomdddRiiiiijatioKeyRRRRRRRRR";
-    public static String TaobaoOrderMessageCount = "tsssbOrdedddrMesiiisageCounttt";
-    public static String TMOrderMessageCount = "tmOrdddsssderMedddssagiiieCount";
-    public static String PaymentMessageCount = "paymendddssstMessdddageCiiiount";
+    public static String KeySetForTmTb= "newRandsssdddiiiiddodddiiiiiKeyNewwwwww";
+    public static String KeySetForRatio = "newdddRssssandiiiomdddRiiiiijatioKeyRRRRRRRRR";
+    public static String TaobaoOrderMessageCount = "tsssbOriiidedddrMesiiisageCounttt";
+    public static String TMOrderMessageCount = "tmOrdddsssderiiiMedddssagiiieCount";
+    public static String PaymentMessageCount = "paymendddssstMeiiissdddageCiiiount";
 }
