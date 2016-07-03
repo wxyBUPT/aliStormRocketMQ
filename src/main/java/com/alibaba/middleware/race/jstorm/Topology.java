ea232e359faebcd59b7be4190a5c2f7f1df2ed39
wxyBUPT <746451950@qq.com>
2016-07-03 16:42:16 +0800

    更改local测试

--- src/main/java/com/alibaba/middleware/race/jstorm/Topology.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/Topology.java
@@ -4,9 +4,9 @@ import backtype.storm.Config;
 import backtype.storm.LocalCluster;
 import backtype.storm.topology.TopologyBuilder;
 import com.alibaba.middleware.race.RaceConfig;
+import com.alibaba.middleware.race.jstorm.bolt.CacheBolt;
 import com.alibaba.middleware.race.jstorm.bolt.MiniutePcMbTradeBolt;
 import com.alibaba.middleware.race.jstorm.bolt.MiniuteTbTmTradeBolt;
-import com.alibaba.middleware.race.jstorm.bolt.PayMessageDeserializeBolt;
 import com.alibaba.middleware.race.jstorm.rocket.RocketSpout;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -25,8 +25,7 @@ public class Topology {
     private static String ORDERMESSAGE_WRITE_BOLT_ID = "orderMessageWriteBolt";
 
     private static String TOPOLOGY_NAME = RaceConfig.JstormTopologyName;
-    private static String PAYMENTMESSAGE_DESERIALIZE_BOLT_ID
-            = "paymentMessageDeserializeBolt";
+    private static String CACHEBOLTID = "cacheBolt";
     private static String CALCULATERATIO_BOLT_ID = "calculateRatioBolt";
     private static String MINIUTETBTMTRADEBOLT_ID = "minuteTbTmTradeBolt" ;
     private static String MINIUTEPCMBTRADEBOLT_ID = "minutePcMbTradeBolt";
@@ -50,26 +49,27 @@ public class Topology {
         RocketSpout rocketSpout = new RocketSpout(
                 allTopic,
                 RaceConfig.MetaConsumerGroup,
-                36
+                1
         );
+
+        builder.setSpout(ROCKETSPOUT_ID,rocketSpout,4).setNumTasks(4);
+
+
         System.out.println("我就是想不通");
-        builder.setSpout(ROCKETSPOUT_ID,rocketSpout,1);
-
-
         //解序列化付款信息,同时查看Tair 来自哪个交易平台
-        PayMessageDeserializeBolt payMessageDeserializeBolt =
-                new PayMessageDeserializeBolt();
-        builder.setBolt(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID,payMessageDeserializeBolt
+        CacheBolt cacheBolt=
+                new CacheBolt();
+        builder.setBolt(CACHEBOLTID,cacheBolt
                 ,4).setNumTasks(1).shuffleGrouping(ROCKETSPOUT_ID);
 
 
         //计算每分钟不同平台交易额比例的bolt
         MiniuteTbTmTradeBolt miniuteTbTmTradeBolt = new MiniuteTbTmTradeBolt();
-        builder.setBolt(MINIUTETBTMTRADEBOLT_ID,miniuteTbTmTradeBolt,1).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
+        builder.setBolt(MINIUTETBTMTRADEBOLT_ID,miniuteTbTmTradeBolt,1).shuffleGrouping(CACHEBOLTID);
 
         //每分钟不同客户端交易额计算的bolt
         MiniutePcMbTradeBolt miniutePcMbTradeBolt = new MiniutePcMbTradeBolt();
-        builder.setBolt(MINIUTEPCMBTRADEBOLT_ID,miniutePcMbTradeBolt,1).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
+        builder.setBolt(MINIUTEPCMBTRADEBOLT_ID,miniutePcMbTradeBolt,1).shuffleGrouping(CACHEBOLTID);
 
         return builder;
     }
