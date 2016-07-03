a525f54f5fd39d29db57b2edc2132543ee605a33
wxyBUPT <746451950@qq.com>
2016-07-03 09:41:37 +0800

    同样是增加对机器资源利用率的尝试

--- src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
+++ src/main/java/com/alibaba/middleware/race/jstorm/RaceTopology.java
@@ -61,23 +61,23 @@ public class RaceTopology {
                 RaceConfig.MetaConsumerGroup,
                 1
         );
-        builder.setSpout(ROCKETSPOUT_ID,rocketSpout,2).setNumTasks(2);
+        builder.setSpout(ROCKETSPOUT_ID,rocketSpout,3).setNumTasks(2);
 
 
         //解序列化付款信息,同时查看Tair 来自哪个交易平台
         PayMessageDeserializeBolt payMessageDeserializeBolt =
                 new PayMessageDeserializeBolt();
         builder.setBolt(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID,payMessageDeserializeBolt
-                ,2).setNumTasks(2).shuffleGrouping(ROCKETSPOUT_ID);
+                ,3).setNumTasks(2).shuffleGrouping(ROCKETSPOUT_ID);
 
 
         //计算每分钟不同平台交易额比例的bolt
         MiniuteTbTmTradeBolt miniuteTbTmTradeBolt = new MiniuteTbTmTradeBolt();
-        builder.setBolt(MINIUTETBTMTRADEBOLT_ID,miniuteTbTmTradeBolt,1).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
+        builder.setBolt(MINIUTETBTMTRADEBOLT_ID,miniuteTbTmTradeBolt,2).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
 
         //每分钟不同客户端交易额计算的bolt
         MiniutePcMbTradeBolt miniutePcMbTradeBolt = new MiniutePcMbTradeBolt();
-        builder.setBolt(MINIUTEPCMBTRADEBOLT_ID,miniutePcMbTradeBolt,1).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
+        builder.setBolt(MINIUTEPCMBTRADEBOLT_ID,miniutePcMbTradeBolt,2).shuffleGrouping(PAYMENTMESSAGE_DESERIALIZE_BOLT_ID);
 
         return builder;
     }
